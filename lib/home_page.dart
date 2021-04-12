850777c9811468458836fd19218b027dafefc757
PawankumarPK <pk9910765616@gmail.com>
2021-04-13 00:41:28 +0530

    button added

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
 }
 
-  class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -13,8 +14,27 @@ class HomePage extends StatefulWidget {
         title: new Text("Calculator App"),
       ),
       body: new Container(
-        child: new Center(
-          child: new Text("Calculator App home"),
+        padding: const EdgeInsets.all(20.0),
+        child: new Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            new TextField(
+              keyboardType: TextInputType.number,
+              decoration: new InputDecoration(hintText: "Enter number 1"),
+            ),
+            new TextField(
+              keyboardType: TextInputType.number,
+              decoration: new InputDecoration(hintText: "Enter number 2"),
+            ),
+            new Padding(padding: const EdgeInsets.only(top: 20.0)),
+            new Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                new RaisedButton(child: new Text("+"), onPressed: () {}),
+                new RaisedButton(child: new Text("-"), onPressed: () {})
+              ],
+            )
+          ],
         ),
       ),
     );
